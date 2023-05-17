7ea7d30766bbbb7c42d512818339ec2f9ff35fc8
Anders Björklund <anders@hackney.se>
2023-05-18 01:08:56 +0200

    Describing how templating should be done + listing available types under users .init directory

--- src/main/java/se/hackney/init/App.java
+++ src/main/java/se/hackney/init/App.java
@@ -1,13 +1,34 @@
 package se.hackney.init;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.File;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        for (String string : args) {
+            System.out.println( string );
+        }
+
+        String home = System.getProperty("user.home");
+        System.out.println("[ home ] = " + home);
+
+        String types = home + File.separator + ".init" + File.separator + "types";
+        System.out.println("[ types ] = " + types);
+
+        File[] directories = new File( types ).listFiles(File::isDirectory);
+        
+        // Find all available types
+        for (File type : directories) {
+            System.out.println( type );
+        }
+
+        // Find ( case insensitive ) type to use
+
+        // Recurse into type, recreating directory structure from type inside current director
+        // Copy all files at current level. Replace all template values in the files using values from parameters ( or secondarily using values from .defaults in type root )
+
     }
+
+
 }
