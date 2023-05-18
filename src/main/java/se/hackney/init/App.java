28ce896a4c52071ceb46c256127ae8f112a91257
Anders Björklund <anders@hackney.se>
2023-05-18 20:25:22 +0200

    Incrementally approaching first init

--- src/main/java/se/hackney/init/App.java
+++ src/main/java/se/hackney/init/App.java
@@ -1,35 +1,71 @@
 package se.hackney.init;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import se.hackney.init.internal.Type;
 
 public class App {
 
     public static void main(String[] args) {
 
+        // Validate input format
+
+        String currentDir = System.getProperty("user.dir");
+        System.out.println( "CURRENT: " + currentDir );
+
         for (String string : args) {
-            System.out.println( string );
+            System.out.println(string);
         }
 
         String home = System.getProperty("user.home");
         System.out.println("[ home ] = " + home);
 
-        String types = home + File.separator + ".init" + File.separator + "types";
-        System.out.println("[ types ] = " + types);
+        String typeHome = home + File.separator + ".init" + File.separator + "types";
+        System.out.println("[ types ] = " + typeHome);
 
-        File[] directories = new File( types ).listFiles(File::isDirectory);
-        
-        // Find all available types
-        for (File type : directories) {
-            System.out.println( type );
-        }
+        Map<String, Type> types = getTypes(typeHome);
+        String target = args[0].toUpperCase();
+
+        System.out.println("TARGET: " + target);
+        Type matchedtype = types.get(target);
+
+        System.out.println("MATCHED: " + matchedtype.getPath());
+
 
         // Find ( case insensitive ) type to use
 
-        // Recurse into type, recreating directory structure from type inside current director
-        // Copy all files at current level. Replace all template values in the files using values from parameters ( or secondarily using values from .defaults in type root )
-        // Only write files if not already present in directory. Use --force flag in order to overwrite already existing files.
+        // Recurse into type, recreating directory structure from type inside current
+        // director
+        // Copy all files at current level. Replace all template values in the files
+        // using values from parameters ( or secondarily using values from .defaults in
+        // type root )
+        // Only write files if not already present in directory. Use --force flag in
+        // order to overwrite already existing files.
 
     }
 
+    private static Map<String, Type> getTypes(String... paths) {
+
+        File[] directories = new File(paths[0]).listFiles(File::isDirectory);
+        Map<String, Type> map = new HashMap<>();
+
+        // Find all available types
+        for (File file : directories) {
+            String path = file.toString();
+            String name = path.substring(path.lastIndexOf(File.separator) + 1).toUpperCase();
+            System.out.println( name );
+
+            Type type = new Type();
+            type.setName(name);
+            type.setPath(path);
+
+            map.put(name, type);
+        }
+
+        return map;
+
+    }
 
 }
---
+++ src/main/java/se/hackney/init/internal/Type.java
@@ -0,0 +1,13 @@
+package se.hackney.init.internal;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Type {
+    
+    private String name;
+    private String path;
+
+}
